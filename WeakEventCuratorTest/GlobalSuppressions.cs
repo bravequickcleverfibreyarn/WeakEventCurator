30dbeb6b42b54013d1ed94bd96899a74aa525aa4
Jirka <Jiri.Cepelka@Volny.cz>
2022-01-21 18:49:42 +0100

    Project infrastructure

---
+++ WeakEventCuratorTest/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "type", Target = "~T:WeakEventCuratorTest.WeakHandlerTest.WeakHandlerTests")]
+[assembly: SuppressMessage("Usage", "CA2201:Do not raise reserved exception types", Justification = "<Pending>", Scope = "member", Target = "~M:WeakEventCuratorTest.WeakHandlerTest.WeakHandlerTestsAide.Target.Exception")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:WeakEventCuratorTest.WeakHandlerTest.WeakHandlerTests.Invoke_TargetIsDead_NothingHappens")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:WeakEventCuratorTest.WeakHandlerTest.WeakHandlerTests.Invoke_TargetIsAlive_HandlerIvoked")]
