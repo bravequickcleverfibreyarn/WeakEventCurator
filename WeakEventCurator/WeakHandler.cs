0cab71e358a26ce38478f2e251824649e8420977
Jirka <Jiri.Cepelka@Volny.cz>
2022-01-21 18:49:42 +0100

    Some preparations

--- WeakEventCurator/WeakHandler.cs
+++ WeakEventCurator/WeakHandler.cs
@@ -2,32 +2,29 @@
 using System;
 using System.Reflection;
 
-namespace Software9119.WeakEvent
+namespace Software9119.WeakEvent;
+
+class WeakHandler
 {
-  internal class WeakHandler
+  readonly WeakReference weakTarget;
+  readonly MethodInfo handlerInfo;
+
+  public WeakHandler ( Delegate handler )
   {
-    private readonly WeakReference weakTarget;
-    private readonly MethodInfo handlerInfo;
+    weakTarget = new WeakReference (handler.Target);
+    handlerInfo = handler.Method;
+  }
 
-    public WeakHandler ( Delegate handler )
-    {
-      weakTarget = new WeakReference (handler.Target);
-      handlerInfo = handler.Method;
-    }
+  public bool IsAlive => weakTarget.IsAlive || handlerInfo.IsStatic;
 
-    public bool IsAlive => weakTarget.IsAlive || handlerInfo.IsStatic;
+  public bool Equals ( Delegate del ) => weakTarget.Target == del.Target && handlerInfo.Name == del.Method.Name;
 
-    public bool Equals ( Delegate del ) => weakTarget.Target == del.Target && handlerInfo.Name == del.Method.Name;
+  public void Invoke ( params object [] parameters )
+  {
+    object? target = weakTarget.Target;
+    if (target is null && !handlerInfo.IsStatic)
+      return;
 
-    public void Invoke ( params object [] parameters )
-    {
-      object target = weakTarget.Target;
-      if (target is null && !handlerInfo.IsStatic)
-      {
-        return;
-      }
-
-      handlerInfo.Invoke (target, parameters);
-    }
+    _ = handlerInfo.Invoke (target, parameters);
   }
 }
--- WeakEventCuratorTest/WeakHandlerTest/WeakHandlerTests.cs
+++ WeakEventCuratorTest/WeakHandlerTest/WeakHandlerTests.cs
@@ -5,163 +5,147 @@ using Software9119.WeakEvent;
 using System;
 using System.Reflection;
 
-namespace WeakEventCuratorTest.WeakHandlerTest
+namespace WeakEventCuratorTest.WeakHandlerTest;
+
+[TestClass]
+public class WeakHandlerTests
 {
-  [TestClass]
-  public class WeakHandlerTests
+  #region IsAlive
+
+  [TestMethod]
+  public void IsAlive_NoReferenceToTarget_IsCollected ()
   {
-    #region IsAlive
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler weakHandler = aide.WeakHandler_NewTarget_Handler1();
 
-    [TestMethod]
-    public void IsAlive_NoReferenceToTarget_IsCollected ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler weakHandler = aide.WeakHandler_NewTarget_Handler1();
+    GC.Collect ();
 
-      GC.Collect ();
+    Assert.IsFalse (weakHandler.IsAlive);
 
-      Assert.IsFalse (weakHandler.IsAlive);
+    // Compiler verification
 
-      // Compiler verification
+    weakHandler = aide.WeakHandler_ExistingTarget_Handler1 ();
+    GC.Collect ();
 
-      weakHandler = aide.WeakHandler_ExistingTarget_Handler1 ();
-      GC.Collect ();
-
-      Assert.IsTrue (weakHandler.IsAlive);
-    }
-
-    [TestMethod]
-    public void IsAlive_ReferenceToTargetExists_IsNotCollected ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      var weakHandler = aide.WeakHandler_ExistingTarget_Handler1();
-
-      GC.Collect ();
-
-      Assert.IsTrue (weakHandler.IsAlive);
-    }
-
-    [TestMethod]
-    public void IsAlive_StaticHandler_ConsideredAsAlive ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler weakHandler = aide.WeakHandler_StaticHandler_StaticException();
-
-      Assert.IsTrue (weakHandler.IsAlive);
-    }
-
-    #endregion
-    #region Equals
-
-    [TestMethod]
-    public void Equals_TargetIsDead_NotEqual ()
-    {
-      var aide = new WeakHandlerTestsAide();
-
-      WeakHandler gauge = aide.WeakHandler_NewTarget_Handler1();
-      GC.Collect ();
-
-      Delegate comparand = aide.Delegate_ExistingTarget_Handler1;
-
-      Assert.IsFalse (gauge.Equals (comparand));
-    }
-
-    [TestMethod]
-    public void Equals_DifferentHandler_NotEqual ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler gauge = aide.WeakHandler_ExistingTarget_Handler1();
-      Delegate comparand = aide.Delegate_ExistingTarget_Handler2;
-
-      Assert.IsFalse (gauge.Equals (comparand));
-    }
-
-    [TestMethod]
-    public void Equals_DifferentTarget_NotEqual ()
-    {
-      WeakHandler gauge;
-      {
-        var aide1 = new WeakHandlerTestsAide();
-        gauge = aide1.WeakHandler_ExistingTarget_Handler1 ();
-      }
-
-      Delegate comparand;
-      {
-        var aide2 = new WeakHandlerTestsAide();
-        comparand = aide2.Delegate_ExistingTarget_Handler1;
-      }
-
-      Assert.IsFalse (gauge.Equals (comparand));
-    }
-
-    [TestMethod]
-    public void Equals_ComparandIsTheSame_Equal ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler gauge = aide.WeakHandler_ExistingTarget_Handler1();
-      Delegate comparand = aide.Delegate_ExistingTarget_Handler1;
-
-      Assert.IsTrue (gauge.Equals (comparand));
-    }
-
-    #endregion
-    #region Invoke
-
-    [TestMethod]
-    public void Invoke_TargetIsDead_NothingHappens ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler wh = aide.WeakHandler_NewTarget_Exception();
-
-      GC.Collect ();
-
-      try
-      {
-        wh.Invoke ();
-      }
-      catch
-      {
-        Assert.IsTrue (false);
-      }
-
-      Assert.IsTrue (true);
-    }
-
-    [TestMethod]
-    public void Invoke_TargetIsAlive_HandlerIvoked ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler wh = aide.WeakHandler_ExistingTarget_Exception();
-
-      Assert.ThrowsException<TargetInvocationException> (() => wh.Invoke ());
-      try
-      {
-        wh.Invoke ();
-      }
-      catch (TargetInvocationException tie)
-      {
-        Assert.AreEqual (WeakHandlerTestsAide.Target.ExceptionMessage, tie.InnerException.Message);
-      }
-    }
-
-    [TestMethod]
-    public void Invoke_StaticHandler_HandlerIvoked ()
-    {
-      var aide = new WeakHandlerTestsAide();
-      WeakHandler wh = aide.WeakHandler_StaticHandler_StaticException();
-
-      Assert.ThrowsException<TargetInvocationException> (() => wh.Invoke ());
-
-      try
-      {
-        wh.Invoke ();
-      }
-      catch (TargetInvocationException tie)
-      {
-        Assert.AreEqual (WeakHandlerTestsAide.Target.ExceptionMessage, tie.InnerException.Message);
-      }
-    }
-
-    #endregion
+    Assert.IsTrue (weakHandler.IsAlive);
   }
+
+  [TestMethod]
+  public void IsAlive_ReferenceToTargetExists_IsNotCollected ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler weakHandler = aide.WeakHandler_ExistingTarget_Handler1();
+
+    GC.Collect ();
+
+    Assert.IsTrue (weakHandler.IsAlive);
+  }
+
+  [TestMethod]
+  public void IsAlive_StaticHandler_ConsideredAsAlive ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler weakHandler = aide.WeakHandler_StaticHandler_StaticException();
+
+    Assert.IsTrue (weakHandler.IsAlive);
+  }
+
+  #endregion
+  #region Equals
+
+  [TestMethod]
+  public void Equals_TargetIsDead_NotEqual ()
+  {
+    var aide = new WeakHandlerTestsAide();
+
+    WeakHandler gauge = aide.WeakHandler_NewTarget_Handler1();
+    GC.Collect ();
+
+    Delegate comparand = aide.Delegate_ExistingTarget_Handler1;
+
+    Assert.IsFalse (gauge.Equals (comparand));
+  }
+
+  [TestMethod]
+  public void Equals_DifferentHandler_NotEqual ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler gauge = aide.WeakHandler_ExistingTarget_Handler1();
+    Delegate comparand = aide.Delegate_ExistingTarget_Handler2;
+
+    Assert.IsFalse (gauge.Equals (comparand));
+  }
+
+  [TestMethod]
+  public void Equals_DifferentTarget_NotEqual ()
+  {
+    WeakHandler gauge;
+    {
+      var aide1 = new WeakHandlerTestsAide();
+      gauge = aide1.WeakHandler_ExistingTarget_Handler1 ();
+    }
+
+    Delegate comparand;
+    {
+      var aide2 = new WeakHandlerTestsAide();
+      comparand = aide2.Delegate_ExistingTarget_Handler1;
+    }
+
+    Assert.IsFalse (gauge.Equals (comparand));
+  }
+
+  [TestMethod]
+  public void Equals_ComparandIsTheSame_Equal ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler gauge = aide.WeakHandler_ExistingTarget_Handler1();
+    Delegate comparand = aide.Delegate_ExistingTarget_Handler1;
+
+    Assert.IsTrue (gauge.Equals (comparand));
+  }
+
+  #endregion
+  #region Invoke
+
+  [TestMethod]
+  public void Invoke_TargetIsDead_NothingHappens ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler wh = aide.WeakHandler_NewTarget_Exception();
+
+    GC.Collect ();
+
+    try
+    {
+      wh.Invoke ();
+    }
+    catch
+    {
+      Assert.IsTrue (false);
+    }
+
+    Assert.IsTrue (true);
+  }
+
+  [TestMethod]
+  public void Invoke_TargetIsAlive_HandlerIvoked ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler wh = aide.WeakHandler_ExistingTarget_Exception();
+
+    TargetInvocationException tie = Assert.ThrowsException<TargetInvocationException> (() => wh.Invoke ());
+    Assert.AreEqual (WeakHandlerTestsAide.Target.ExceptionMessage, tie.InnerException.Message);
+  }
+
+  [TestMethod]
+  public void Invoke_StaticHandler_HandlerIvoked ()
+  {
+    var aide = new WeakHandlerTestsAide();
+    WeakHandler wh = aide.WeakHandler_StaticHandler_StaticException();
+
+    TargetInvocationException tie = Assert.ThrowsException<TargetInvocationException> (() => wh.Invoke ());
+    Assert.AreEqual (WeakHandlerTestsAide.Target.ExceptionMessage, tie.InnerException.Message);
+  }
+
+  #endregion
 }
