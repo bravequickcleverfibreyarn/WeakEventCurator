e3efae43dac229ad97108141794bc7180cb53ed8
Jirka <Jiri.Cepelka@Volny.cz>
2022-01-21 18:49:45 +0100

    WeakEventCuratorTests

---
+++ WeakEventCuratorTest/WeakEventCuratorTest/WeakEventCuratorTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Software9119.WeakEvent;
+
+using System;
+
+namespace WeakEventCuratorTest.WeakEventCuratorTest;
+
+[TestClass]
+sealed public class WeakEventCuratorTests
+{
+  [TestMethod]
+  public void CleanUpConstruction_IsNull__ThrowsArgumentNullException ()
+  {
+    _ = Assert.ThrowsException<ArgumentNullException>
+    (
+      () => new WeakEventCurator ( null! )
+    );
+  }
+
+  [TestMethod]
+  public void CleanUpConstruction_ReturnsNull__ThrowsArgumentException ()
+  {
+    _ = Assert.ThrowsException<ArgumentException>
+    (
+      () => new WeakEventCurator ( x => null! )
+    );
+  }
+}
