4f17bab4078696e99b5a5a89c6fdaace9032520f
Jirka <Jiri.Cepelka@Volny.cz>
2022-01-21 18:49:43 +0100

    WeakEventCuratorTests.Add

---
+++ WeakEventCuratorTest/WeakEventCuratorTest/Abstract/WeakEventCuratorTests.Shared.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Software9119.WeakEvent;
+
+using System;
+
+namespace WeakEventCuratorTest.WeakEventCuratorTest.Abstract;
+
+abstract public class WeakEventCuratorTests_Shared
+{
+
+  // Class  set up
+
+  [ClassInitialize]
+  static public void Init ( TestContext _ ) => WeakEventCurator = new WeakEventCurator ( default );
+  [ClassCleanup]
+  static public void Cleansing () => WeakEventCurator.Dispose ();
+
+  // Protected requirement
+
+  protected delegate void AddRemoveMethod ( object eventSource, string eventName, params Delegate [] handlers );
+  abstract protected AddRemoveMethod OneOfAddRemoveMethods { get; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+  static protected WeakEventCurator WeakEventCurator { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+  // Execution
+
+  [TestMethod]
+  public void EventSource_IsNull__ThrowsArgumentNullException ()
+  {
+    _ = Assert.ThrowsException<ArgumentNullException> ( () => OneOfAddRemoveMethods ( null!, "", string.IsNullOrWhiteSpace ) );
+  }
+
+  [TestMethod]
+  public void EventName_IsNull__ThrowsArgumentNullException ()
+  {
+    _ = Assert.ThrowsException<ArgumentNullException> ( () => OneOfAddRemoveMethods ( new object (), null!, string.IsNullOrWhiteSpace ) );
+  }
+
+  [TestMethod]
+  public void Handlers_IsNull__ThrowsArgumentNullException ()
+  {
+    _ = Assert.ThrowsException<ArgumentNullException> ( () => OneOfAddRemoveMethods ( new object (), "", null! ) );
+  }
+
+  [TestMethod]
+  public void Handlers_IsEmpty__ThrowsArgumentException ()
+  {
+    _ = Assert.ThrowsException<ArgumentException> ( () => OneOfAddRemoveMethods ( new object (), "", new Delegate [0] ) );
+  }
+
+  [TestMethod]
+  public void Handlers_HasNullDelegate__ThrowsArgumentException ()
+  {
+    _ = Assert.ThrowsException<ArgumentException> ( () => OneOfAddRemoveMethods ( new object (), "", string.IsNullOrWhiteSpace, null! ) );
+  }
+}
---
+++ WeakEventCuratorTest/WeakEventCuratorTest/WeakEventCuratorTests.Add.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WeakEventCuratorTest.WeakEventCuratorTest.Abstract;
+
+namespace WeakEventCuratorTest.WeakEventCuratorTest;
+
+[TestClass]
+public class WeakEventCuratorTests_Add : WeakEventCuratorTests_Shared
+{
+  [ClassInitialize]
+  static new public void Init ( TestContext tc ) => WeakEventCuratorTests_Shared.Init ( tc );
+  [ClassCleanup]
+  static new public void Cleansing () => WeakEventCuratorTests_Shared.Cleansing ();
+
+  override protected AddRemoveMethod OneOfAddRemoveMethods => WeakEventCurator.Add;
+
+  [TestMethod]
+  public void AddSomeHandler__Added () => WeakEventCurator.Add ( new object (), "", string.Intern );
+}
