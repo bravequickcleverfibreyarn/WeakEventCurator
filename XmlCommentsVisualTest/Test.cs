bf62d3d1c83f56a89841e895b8287029eb5f49bc
Jirka <Jiri.Cepelka@Volny.cz>
2022-01-29 17:09:08 +0100

    XmlCommentsVisualTest

---
+++ XmlCommentsVisualTest/Test.cs
@@ -0,0 +1,4 @@
+﻿using AAAA = Software9119.WeakEvent.WeakEventCurator;
+using BBBB = Software9119.WeakEvent.WeakHandler;
+using CCCC = Software9119.WeakEvent.WeakHandlerCleanUp;
+using DDDD = Software9119.WeakEvent.WeakHandlerFacility;
